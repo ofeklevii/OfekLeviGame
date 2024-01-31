3009f34f3877e004270e9c0019eec787f81a59e8
ofeklevii <ofeklevi@sisma.org.il>
2024-01-31 15:53:14 +0200

    first commit

--- app/src/main/java/com/example/ofeklevigame/MainActivity.java
+++ app/src/main/java/com/example/ofeklevigame/MainActivity.java
@@ -3,9 +3,24 @@ package com.example.ofeklevigame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tV1;
+    private TextView tV2;
+    private TextView tV3;
+    private TextView tV4;
+    private TextView tV5;
+    private TextView tV6;
+    private EditText eT1;
+    private EditText eT2;
+    private EditText eT3;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
